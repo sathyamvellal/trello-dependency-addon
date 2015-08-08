796126a500a81822e7860c3dc68efab3b48823cb
Sathyam Vellal <sathyam.vellal@gmail.com>
2015-08-08 22:09:23 +0530

    Initial Commit for chrome-extension files

---
+++ board.js
@@ -0,0 +1,7 @@
+var boardMain = function() {
+  // begin main
+  alert('You opened a board');
+}
+
+// on board load
+boardMain();
---
+++ card.js
@@ -0,0 +1,7 @@
+var cardMain = function() {
+  // begin main
+  alert('You opened a card');
+}
+
+// on card load
+cardMain();
